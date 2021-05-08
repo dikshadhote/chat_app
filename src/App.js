a7efaf7be8461f74d521a6acbedf41870f452a1a
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-05-08 15:31:12 +0530

    bug: module not found

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Message from "./Message";
+import db from "./firebase";
 
 function App() {
   const [input, setInput] = useState("");
-  const [message, setmessage] = useState(["hi", "hello"]);
-
-  console.log(input);
-  console.log(message);
-
+  const [message, setMessage] = useState([
+  ]);
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
+//
+useEffect(()=>{
+  db.collection('messages').onSnapshot(snapshot=>{
+    setMessage(snapshot.docs.map(doc => doc.data()))
+  })
+},[]);
   const sendMessage = (event) => {
     //...message appends text at the end of array
     event.preventDefault();
-    setmessage([...message, input]);
+    setMessage([...message, { username: username, text: input }]);
     setInput("");
   };
 
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <form>
-          <h1>Messenger🕊️</h1>
-          <FormControl>
-            <InputLabel>Enter a message...</InputLabel>
-            <Input
-              value={input}
-              onChange={(event) => setInput(event.target.value)}
-            />
-            <Button
-              type="submit"
-              disabled={!input}
-              onClick={sendMessage}
-              variant="contained"
-              color="primary"
-            >
-              send message
-            </Button>
-          </FormControl>
-        </form>
+        <div  style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}} >
+          <form >
+            <h1>Messenger🕊️</h1>
+            <h2>Welcome {username}</h2>
+            <FormControl>
+              <InputLabel>Enter a message...</InputLabel>
+              <Input
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
+              />
+              <Button
+                type="submit"
+                disabled={!input}
+                onClick={sendMessage}
+                variant="contained"
+                color="primary"
+              >
+                send message
+              </Button>
+            </FormControl>
+          </form>
+        </div>
         {message.map((message) => (
-          <p>{message}</p>
+          <Message username={username} message={message} />
         ))}
       </header>
     </div>
